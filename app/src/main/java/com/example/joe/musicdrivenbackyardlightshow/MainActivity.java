dbffdcfadb132375186b0fa048f2408b93a94d9d
jguinta <joeyguinta@gmail.com>
2015-09-16 18:37:47 -0400

    Spotify API initial findings
    Checkl out the spotify web api (https://developer.spotify.com/web-api/)
    and android sdk
    (https://developer.spotify.com/technologies/spotify-android-sdk/)

---
+++ app/src/main/java/com/example/joe/musicdrivenbackyardlightshow/MainActivity.java
@@ -0,0 +1,197 @@
+package com.example.joe.musicdrivenbackyardlightshow;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
+import com.spotify.sdk.android.player.Spotify;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerNotificationCallback;
+import com.spotify.sdk.android.player.PlayerState;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import companydomain.applicationname.R;
+
+public class MainActivity extends Activity implements
+        PlayerNotificationCallback, ConnectionStateCallback, OnClickListener {
+
+    // TODO: Replace with your client ID
+    private static final String CLIENT_ID = "7229dfb6591f476bacb0f4cd936c7061";
+    // TODO: Replace with your redirect URI
+    private static final String REDIRECT_URI = "spring-light-show-login://callback";
+
+    private Player mPlayer;
+
+    // Request code that will be used to verify if the result comes from correct activity
+    // Can be any integer
+    private static final int REQUEST_CODE = 1337;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
+                AuthenticationResponse.Type.TOKEN,
+                REDIRECT_URI);
+        builder.setScopes(new String[]{"user-read-private", "streaming"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+
+        Button queryButton = (Button) findViewById(R.id.queryButton);
+        // because we implement OnClickListener we only have to pass "this"
+        // (much easier)
+        queryButton.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
+                mPlayer = Spotify.getPlayer(playerConfig, this, new Player.InitializationObserver() {
+                    @Override
+                    public void onInitialized(Player player) {
+                        mPlayer.addConnectionStateCallback(MainActivity.this);
+                        mPlayer.addPlayerNotificationCallback(MainActivity.this);
+                      //  mPlayer.play("spotify:track:2TpxZ7JUBn3uw46aR7qd6V");
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
+                    }
+                });
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        // detect the view that was "clicked"
+        switch (view.getId()) {
+            case R.id.queryButton:
+                new RetrieveSongTask(((EditText) findViewById(R.id.song)).getText().toString()).execute();
+                break;
+        }
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d("MainActivity", "User logged in");
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d("MainActivity", "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(Throwable error) {
+        Log.d("MainActivity", "Login failed");
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("MainActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("MainActivity", "Received connection message: " + message);
+    }
+
+    @Override
+    public void onPlaybackEvent(EventType eventType, PlayerState playerState) {
+        Log.d("MainActivity", "Playback event received: " + eventType.name());
+    }
+
+    @Override
+    public void onPlaybackError(ErrorType errorType, String errorDetails) {
+        Log.d("MainActivity", "Playback error received: " + errorType.name());
+    }
+
+    @Override
+    protected void onDestroy() {
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
+    }
+
+
+    class RetrieveSongTask extends AsyncTask<Void, Void, String> {
+        private String API_URL = "https://api.spotify.com/v1/search";
+       // private String API_KEY = "?q=tania%20bowra&type=artist\"";
+        private Exception exception;
+
+
+        private String song;
+
+        public RetrieveSongTask(String song) {
+            this.song = song;
+        }
+
+        protected void onPreExecute() {
+            findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
+            ((TextView)findViewById(R.id.responseView)).setText("");
+        }
+
+        protected String doInBackground(Void... urls) {
+
+            // Do some validation here
+
+            try {
+                URL url = new URL(API_URL + "?q=" + song + "&type=track");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            if(response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            findViewById(R.id.progressBar).setVisibility(View.GONE);
+            Log.i("INFO", response);
+            TextView responseView = (TextView) findViewById(R.id.responseView);
+            responseView.setText(response);
+        }
+    }
+}
