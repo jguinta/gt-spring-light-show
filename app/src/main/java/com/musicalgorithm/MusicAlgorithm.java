8c71fc4c501eb37e25ae052141068b7603997f1b
michael-leon <mleon@gatech.edu>
2015-11-05 14:14:51 -0500

    Separated getAverageAmplitude method and setOpacity method.

--- app/src/main/java/com/musicalgorithm/MusicAlgorithm.java
+++ app/src/main/java/com/musicalgorithm/MusicAlgorithm.java
@@ -5,7 +5,7 @@ package com.musicalgorithm;
  */
 public class MusicAlgorithm {
 
-    public static float[] getAmplitude(short[] inputStream) {
+    public static float getAverageAmplitude(short[] inputStream) {
         float[] mag = new float[inputStream.length / 2];
         float average = 0f;
         for (int i = 0; i < inputStream.length - 1; i += 2) {
@@ -13,17 +13,27 @@ public class MusicAlgorithm {
             average += mag[i];
         }
         average /= mag.length;
-        float[] opac = new float[mag.length];
-        for(int i = 0; i < mag.length; i++){
-            opac[i]=0.5f;
-        }
-        for (int i = 0; i < mag.length; i++) {
-            mag[i] = mag[i] - average;
-            if(!((mag[i]+0.5f  > 1) || (mag[i]+0.5f  < 0))){
-                opac[i]=mag[i];
-            }
-        }
-        return opac;
+        
+        return average;
+    }
+    
+    public static float setOpacity(short[] inputStream, float opacity, float average) {
+        /** Opacity should be a single number, not an array
+         * Opacity gets set to 0.5.
+         *  If the magnitude is greater than average, add some amount of opacity (the magnitude value?).
+         *  If the magnitude is less than average, remove some amount of opacity (the magnitude value?).
+         * If opacity goes above 1, set to 1.
+         * If opacity goes below 0, set to 0.1 (to avoid blackout). */
+         float mag = (Math.abs(inputStream[0]) + Math.abs(inputStream[1])) / 2;
+         mag -= average;
+         opacity += mag;
+         if(opacity <= 0) {
+             opacity = 0.1;
+         }
+         else if(opacity > 1) {
+             opacity = 1;
+         }
+         return opacity;    
     }
 
 
